adea7df4db9745b12adf2e8dc39b614e765bea91
elyahyaoui-oubay <elyahyaoui.oubay@gmail.com>
2021-12-23 11:11:56 +0100

    add egale

--- Live-Coding/script.js
+++ Live-Coding/script.js
@@ -2,10 +2,15 @@
 var num1ChaineCa;
 var num1;
 var operation;
+var num2ChaineCa;
+var num2;
+var total;
 
 function onClickNombre(nombre){
     if(num1ChaineCa == undefined){
         num1ChaineCa = nombre;
+    }else{
+        num2ChaineCa = nombre;
     }
     afficher();
 }
@@ -13,13 +18,20 @@ function onClickNombre(nombre){
 function afficher(nombre){
     var afficher = document.getElementById("afficher");
         afficher.value = '';
-
-    if(num1ChaineCa != undefined){
-        afficher.value += num1ChaineCa;
+    if(num1 != undefined && num2 != undefined && operation != undefined){
+        afficher.value = nombre;
+    }else{
+        if(num1ChaineCa != undefined){
+            afficher.value += num1ChaineCa;
+        }
+        if(operation != undefined){
+            afficher.value += operation;
+        }
+        if(num2ChaineCa != undefined){
+            afficher.value += num2ChaineCa;
+        }
     }
-    if(operation != undefined){
-        afficher.value += operation;
-    }
+    
 }
 
 function Operation(operationPara){
@@ -28,4 +40,26 @@ function Operation(operationPara){
         num1 = parseFloat(num1ChaineCa);
     }
     afficher();
+}
+
+function Egale(){
+    num1 = parseFloat(num1ChaineCa);
+    num2 = parseFloat(num2ChaineCa);
+    total = somme(num1, num2, operation);
+
+    afficher(total);
+}
+
+function somme(num1, num2, operation){
+    var _total = undefined;
+
+    switch(operation){
+        case "+":
+            _total = num1 + num2;
+        break;
+        case "-":
+            _total = num1 - num2;
+        break;
+    }
+    return _total;
 }
