c7dfb6103476d0c89f6afb25db94cabfdaf2e11f
elyahyaoui-oubay <elyahyaoui.oubay@gmail.com>
2021-12-23 10:44:28 +0100

    add folder

--- test/js.js
+++ test/js.js
@@ -1,4 +1,4 @@
-// declaration des variables
+// Variables
 var num1ChaineCa;
 var num2ChaineCa;
 var operation;
@@ -6,78 +6,71 @@ var operation;
 var num1;
 var num2;
 
-var total;
-
-
+var somme;
 
 function clickNombre(nombre){
     if(num1ChaineCa == undefined){
         num1ChaineCa = nombre;
-    }else{
-        if(num2ChaineCa == undefined){
-            num2ChaineCa = nombre;
-        }
     }
-    
+    else{
+        num2ChaineCa = nombre;
+    }
 
     afficher();
 }
 
 function afficher(nombre){
-    var afficher = document.getElementById("afficher")
-        afficher.value = '';
-
-        if (num1 != undefined && num2 != undefined && operation != undefined ){
-            afficher.value = nombre;
-            }else{
-                if(num1ChaineCa != undefined ){
-                    afficher.value += num1ChaineCa;
-                }
-                if(operation != undefined){
-                    afficher.value += operation;
-                }
-                if(num2ChaineCa != undefined){
-                    afficher.value += num2ChaineCa;
-                }
-            }
-            
-        
-        
+    var afficher = document.getElementById("afficher");
+    afficher.value = '';
+    if(num1 != undefined && num2 != undefined && operation != undefined){
+        afficher.value = nombre;
+    }else{
+    if(num1ChaineCa != undefined) {
+        afficher.value += num1ChaineCa;
+    }
+    if(operation != undefined){
+        afficher.value += operation;
+    }
+    if(num2ChaineCa != undefined){
+        afficher.value += num2ChaineCa;
+    } 
+    
+        }
+    
 }
-
-function Operation(operationPara){
-    if (operation == undefined){
-        operation = operationPara
+function OnClickOperation(operationPara){
+    if(operation == undefined){
+        operation = operationPara;
         num1 = parseFloat(num1ChaineCa);
     }
+        
 
-    afficher();
+        afficher();
 }
 
 function Egale(){
     num1 = parseFloat(num1ChaineCa);
     num2 = parseFloat(num2ChaineCa);
 
-    total = somme(num1, num2, operation);
+    somme = calculer(num1, num2, operation)
 
-    afficher(total);
+afficher(somme);
 }
 
-function somme(num1, num2, operation){
+function calculer(num1, num2, operationPara){
+    var _somme = undefined;
 
-    var _total = undefined;
-
-    switch(operation){
+    switch(operationPara){
         case "+":
-            _total = num1 + num2;
-        break;
-
+            _somme = num1 + num2;
+            break;
         case "-":
-            _total = num1 - num2;
-        break;
+            _somme = num1 - num2;
+            break;
     }
 
-    return _total;
+    return _somme;
 }
+
 
 
