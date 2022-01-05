5c77ef838de27e823ef140fc7c003f8de604b56c
elyahyaoui-oubay <elyahyaoui.oubay@gmail.com>
2022-01-05 11:12:41 +0100

    update

--- Prototype-2 OOP/calculatruce.js
+++ Prototype-2 OOP/calculatruce.js
@@ -1,34 +1,34 @@
 // Présentation
-let xString, yString;
+let num1ChaineCa, num2ChaineCa;
 let calculatrice = new CalculatriceBLO();
 
 function ClickNumber(number){
-    if(calculatrice.x == undefined){
-        if(xString == undefined) xString = '';
-        xString += number;
+    if(calculatrice.num1 == undefined){
+        if(num1ChaineCa == undefined) num1ChaineCa = '';
+        num1ChaineCa += number;
     } 
     else {
-        if(yString == undefined) yString = '';
-        yString += number;
+        if(num2ChaineCa == undefined) num2ChaineCa = '';
+        num2ChaineCa += number;
     } 
     Afficher();
 }
 
 function Afficher(number){
 
-    let afficheur = document.getElementById("afficheur");
+    let afficheur = document.getElementBy2Id("afficheur");
     afficheur.value = "";
     
-    if(calculatrice.x != undefined && calculatrice.y != undefined && calculatrice.operation != undefined){
+    if(calculatrice.num1 != undefined && calculatrice.num2 != undefined && calculatrice.operation != undefined){
 
         afficheur.value = number;
     }else{
-        if(xString != undefined)
-        afficheur.value += xString 
+        if(num1ChaineCa != undefined)
+        afficheur.value += num1ChaineCa 
         if(calculatrice.operation != undefined)
             afficheur.value += calculatrice.operation     
-        if(yString != undefined)
-            afficheur.value += yString 
+        if(num2ChaineCa != undefined)
+            afficheur.value += num2ChaineCa 
 
         }
  
@@ -38,7 +38,7 @@ function Afficher(number){
 function Operation(operationParam){
     if(calculatrice.operation == undefined){
         calculatrice.operation = operationParam;
-        calculatrice.x = parseFloat(xString);
+        calculatrice.num1 = parseFloat(num1ChaineCa);
         Afficher();
     }else{
         alert("Vous avez déjà choisi l'opération " + calculatrice.operation);
@@ -46,16 +46,16 @@ function Operation(operationParam){
 }
 
 function Egale(){
-    calculatrice.x = parseFloat(xString);
-    calculatrice.y = parseFloat(yString);
+    calculatrice.num1 = parseFloat(num1ChaineCa);
+    calculatrice.num2 = parseFloat(num2ChaineCa);
     calculatrice.Calculer();
     Afficher(calculatrice.solution);
 }
 
 function Init(){
     calculatrice.Init();
-    xString = undefined;
-    yString = undefined;
-    let afficheur = document.getElementById("afficheur");
+    num1ChaineCa = undefined;
+    num2ChaineCa = undefined;
+    let afficheur = document.getElementBy2Id("afficheur");
     afficheur.value = "";
 }
