f9358994c97052c09906552faec006ab172c83aa
elyahyaoui-oubay <elyahyaoui.oubay@gmail.com>
2022-01-06 15:35:21 +0100

    update

--- Live-Coding-2/calculatriceoop.js
+++ Live-Coding-2/calculatriceoop.js
@@ -9,56 +9,56 @@ class CalculatriceOOP {
 
     // Setters and getters
     get num1() {
-        return this._num1  
+        return this.#_num1  
     }
     set num1(value){
-        this._num1 = value;
+        this.#_num1 = value;
     }
     get num2() {
-        return this._num2 
+        return this.#_num2 
     }
     set num2(value){
-        this._num2 = value;
+        this.#_num2 = value;
     }
     get num2() {
-        return this._num2 
+        return this.#_num2 
     }
     set num2(value){
-        this._num2 = value;
+        this.#_num2 = value;
     }
     get operation() {
-        return this._operation
+        return this.#_operation
     }
     set operation(value){
-          this._operation = value;
+          this.#_operation = value;
     }
 
     get solution() {
-        return this._solution
+        return this.#_solution
     }
     set solution(value){
-          this._solution = value;
+          this.#_solution = value;
     }
 
     Calculer(){
-        this._solution = undefined;
-        switch (this._operation) {
+        this.#_solution = undefined;
+        switch (this.#_operation) {
             case '+':
-                this._solution = this._num1 + this._num2;
+                this.#_solution = this.#_num1 + this.#_num2;
                 break;
             case '-':
-                this. _solution = this._num1- this._num2;
+                this. #_solution = this.#_num1- this.#_num2;
                     break;
             default:
                 break;
         }
-        return this._solution;
+        return this.#_solution;
     }
 
     Init(){
-        this._num1 = undefined;
-        this._num2 = undefined;
-        this._operation = undefined;
+        this.#_num1 = undefined;
+        this.#_num2 = undefined;
+        this.#_operation = undefined;
     }
 
   }
